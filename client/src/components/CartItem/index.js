0339369387815863cd47ba5e25b6c26b28dd56d7
cpkopp <cameronpkopp@gmail.com>
2023-01-24 17:52:41 -1000

    cart actions/reducers all set up

--- client/src/components/CartItem/index.js
+++ client/src/components/CartItem/index.js
@@ -1,8 +1,39 @@
 //DECLARATIONS: REACT ---------------------------
 import React from 'react';
+import { useStoreContext } from '../../utils/GlobalState';
+import { REMOVE_FROM_CART, UPDATE_CART_QUANTITY } from '../../utils/actions';
 
 //CARTITEM COMPONENT ==============================
 const CartItem = ({ item }) => {
+
+    const [, dispatch] = useStoreContext();
+
+    const removeFromCart = item => {
+        dispatch({
+            type: REMOVE_FROM_CART,
+            _id: item._id
+        })
+    }
+
+    const onChange = (e) => {
+        
+        const value = e.target.value;
+
+        if (value === '0') {
+            dispatch({
+                type: REMOVE_FROM_CART,
+                _id: item._id
+            });
+        } else {
+            dispatch({
+                type: UPDATE_CART_QUANTITY,
+                _id: item._id,
+                purchasedQuantity: parseInt(value)
+            });
+        }
+
+    }
+
     return(
         <div className='flex-row'>
             <div>
@@ -18,11 +49,13 @@ const CartItem = ({ item }) => {
                     <input
                         type="number"
                         placeholder="1"
-                        value={item.purchasedQuantity}
+                        value={item.purchaseQuantity}
+                        onChange={onChange}
                     />
                     <span
                         role="img"
                         aria-label="trash"
+                        onClick={() => removeFromCart(item)}
                     >
                         🗑️
                     </span>
