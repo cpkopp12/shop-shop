4b4a38e84d1cc1575055f4f1b8e54769a25858ac
cpkopp <cameronpkopp@gmail.com>
2023-02-10 18:57:14 -1000

    need to fix json token

--- client/src/App.js
+++ client/src/App.js
@@ -48,9 +48,9 @@ function App() {
               <Route exact path="/" component={Home} />
               <Route exact path="/login" component={Login} />
               <Route exact path="/signup" component={Signup} />
+              <Route exact path="/success" component={Success} />
               <Route exact path="/orderHistory" component={OrderHistory} />
               <Route exact path="/products/:id" component={Detail} />
-              <Route exact path="/success" component={Success} />
               <Route component={NoMatch} />
             </Switch>
           </StoreProvider>
